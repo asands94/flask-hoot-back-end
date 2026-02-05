df1abd314f6456b8e749214613c90fb84fabcf73
Ashley Sands <ashley.sands01@gmail.com>
2026-02-05 10:49:56 -0800

    full crud on comments

--- app.py
+++ app.py
@@ -8,22 +8,14 @@ import bcrypt
 from auth_middleware import token_required
 from auth_blueprint import authentication_blueprint
 from hoots_blueprint import hoots_blueprint
+from comments_blueprint import comments_blueprint
 
 load_dotenv()
 
 app = Flask(__name__)
 app.register_blueprint(authentication_blueprint)
 app.register_blueprint(hoots_blueprint)
-
-
-def get_db_connection():
-    connection = psycopg2.connect(
-        host='localhost',
-        database=os.getenv('POSTGRES_DATABASE'),
-        user=os.getenv('POSTGRES_USERNAME'),
-        password=os.getenv('POSTGRES_PASSWORD')
-    )
-    return connection
+app.register_blueprint(comments_blueprint)
 
 
 app.run(debug=True, port=5000)
