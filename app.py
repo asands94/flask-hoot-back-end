e205f15189e5fb4551b43632c203a41b1ff5baf9
Ashley Sands <ashley.sands01@gmail.com>
2026-02-05 10:39:51 -0800

    full CRUD on hoots

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ import psycopg2.extras
 import bcrypt
 from auth_middleware import token_required
 from auth_blueprint import authentication_blueprint
+from hoots_blueprint import hoots_blueprint
 
 load_dotenv()
 
 app = Flask(__name__)
 app.register_blueprint(authentication_blueprint)
+app.register_blueprint(hoots_blueprint)
 
 
 def get_db_connection():
--- auth_middleware.py
+++ auth_middleware.py
@@ -4,23 +4,6 @@ import jwt
 import os
 
 
-# def token_required(f):
-#     @wraps(f)
-#     def decorated_function(*args, **kwargs):
-#         authorization_header = request.headers.get('Authorization')
-#         if authorization_header is None:
-#             return jsonify({"error": "Unauthorized"}), 401
-#         try:
-#             token = authorization_header.split(' ')[1]
-#             token_data = jwt.decode(token, os.getenv(
-#                 'JWT_SECRET'), algorithms=["HS256"])
-# # NOTE: add ['payload'] so the object does not contain payload as a key
-#             g.user = token_data
-#         except Exception as error:
-#             return jsonify({"error": str(error)}), 500
-#         return f(*args, **kwargs)
-#     return decorated_function
-
 def token_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
